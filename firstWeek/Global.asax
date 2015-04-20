df14cb2f9fc3bed8cc7cad820755da5e7b71a50a
frank ray <rayisme@gmail.com>
2015-04-21 06:07:30 +0800

    新建立一個mvc專案

---
+++ firstWeek/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="firstWeek.MvcApplication" Language="C#" %>
---
+++ firstWeek/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(firstWeek.Startup))]
+namespace firstWeek
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
